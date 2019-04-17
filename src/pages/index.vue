5cc838c7d0b0906d29fe5c0ef0c646829bc38b7d
xiaomo <stormxiaomo@gmail.com>
2019-04-18 00:32:40 +0800

    revert to init fork status.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,31 +1,15 @@
 <template>
-   <div>
-      <labelSearchPanel v-bind:permissions="permissions"/>
-      <hr/>
-      <TreeComponent v-bind:permissionData="permissionData"/>
+  <div>
+    {{msg}}
   </div>
 </template>
 
 <script lang="ts">
-  import labelSearchPanel from '../components/labelSearchPanel.vue';
-  import TreeComponent from '../components/TreeSelect/index.vue';
-  import { mockPermissions } from '../../dummies/mockPermissions';
-  import { flatPermissions } from '../utils/flatPermissions';
-
   export default {
     name: "index",
-    components: {
-      labelSearchPanel,
-      TreeComponent
-    },
-    mounted(){
-      this.permissions = mockPermissions;
-      this.permissionData = flatPermissions(mockPermissions);
-    },
     data() {
       return {
-        permissions: {},
-        permissionData: {},
+        msg: "Hello",
       };
     }
   };
--- src/utils/findLabel.ts
+++ src/utils/findLabel.ts
@@ -1,38 +1,6 @@
 import {Permissions} from "../types/Permissions";
 
-/** for quickly solved : loop all of the permissions */
 export function findLabel(code: string, permissions: Permissions): string {
-  for(const permission of permissions){
-    if(code === permission.code) {
-      return permission.label
-    }
-    else {
-      const label = findLabel(code, permission.children)
-      if(label) return label;
-    }
-  }
+  // TODO
   return null;
 }
-
-/** for more performance solved : loop only by index key */
-/*
-const getLabelText = (codeStr, indexes, permissions) => {
-  const permissionData = permissions.find(permission => permission.code === codeStr);
-  const label = !permissionData ? null : permissionData.label;
-  if(indexes.length === 0) return label;
-  
-  const [ index, ...nextIndexes] = indexes;
-  return getLabelText(`${codeStr}-${index}`, nextIndexes, permissionData.children);
-}
-
-export function findLabel(code: string, permissions: Permissions): string {
-  if(!code) return null;
-  const [codeStr, ...indexes] = code.split('-');
-  const permissionData = permissions.find(permission => permission.code === codeStr);
-  const label = !permissionData ? 'unkonw code' : permissionData.label;
-  if(indexes.length === 0) return label;
-  
-  const [ index, ...nextIndexes] = indexes;;
-  return getLabelText(`${codeStr}-${index}`, nextIndexes, permissionData.children);
-}
-*/
