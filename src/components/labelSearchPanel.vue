ab4756ee948430822755210c891a6e249b50a3a3
xiaomo <stormxiaomo@gmail.com>
2019-04-18 00:52:31 +0800

    Implement the findLabel function

---
+++ src/components/labelSearchPanel.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h5>{{"Find Label by Code"}}</h5>
+    <div>
+      <label>Code Input</label>
+      <input
+        type="text"
+        placeholder="input code here"
+        v-model="code"
+      >
+    </div>
+    <div>
+      <label>Result Label</label>
+      <input
+        type="text"
+        placeholder="result here"
+        readonly
+        v-model="getLabel"
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { findLabel } from "../utils/findLabel";
+
+export default {
+  name: "labelSearchPanel",
+  props: ["permissions"],
+  data() {
+    return {
+      code: ""
+    };
+  },
+  computed: {
+    getLabel: function() {
+      const label = findLabel(this.code, this.permissions);
+      return label;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
